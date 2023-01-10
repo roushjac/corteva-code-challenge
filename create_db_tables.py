7f3f7e0eb3f1cc065b25043610d9ae308d2066fd
Jacob Roush <roush.jake@gmail.com>
2023-01-10 18:33:20 -0500

    comment

--- create_db_tables.py
+++ create_db_tables.py
@@ -1,6 +1,7 @@
 # run this to populate the database with tables as defined in our data models
 
 from corteva_app.app import app, db
+# need to import the models or else the db object doesn't know what tables to create
 from corteva_app.database import models
 
 with app.app_context():
