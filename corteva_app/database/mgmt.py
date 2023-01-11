63062d1dd5bb0973aade73bcaf76dbb72834bcd3
Jacob Roush <roush.jake@gmail.com>
2023-01-11 15:41:02 -0500

    notebook for inserting weather successful

--- corteva_app/database/mgmt.py
+++ corteva_app/database/mgmt.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-conn_string = 'postgresql://postgres:password@localhost:5432/corteva-db'
+conn_string = "postgresql://postgres:password@localhost:5432/corteva-db"
 
 engine = create_engine(conn_string)
 # using a scoped session allows us to gracefully close connections when API request finish or the app shuts down
