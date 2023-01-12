5d1f27783509b43ca39f7266eeb3c8e27078f7c0
Jacob Roush <roush.jake@gmail.com>
2023-01-12 12:22:50 -0500

    template out routes

--- corteva_app/app.py
+++ corteva_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from corteva_app.database.mgmt import db_session, conn_string
 
@@ -17,3 +17,56 @@ db.init_app(app)
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
+
+# allow user to specify station id and date
+@app.route("/api/weather", methods=["GET"])
+def get_weather():
+    args = request.args
+    station_id: str = args.get("station_id", type=str)
+    date: str = args.get("date", type=str)
+
+    if None not in (station_id, date):
+        # both paremeters are specified
+        pass
+    elif station_id:
+        pass
+    elif date:
+        pass
+    else:
+        # return empty result if nothing is specified - too much data to return everything
+        result = {}
+
+    return result
+
+# allow user to specify year
+@app.route("/api/yield", methods=["GET"])
+def get_yield():
+    args = request.args
+    year: int = args.get("year", type=int)
+    
+    if year:
+        pass
+    else:
+        # return everything from the table
+        result = {}
+
+    return result
+
+# allow user to specify station id and year
+@app.route("api/weather/stats", methods=["GET"])
+def get_weather_stats():
+    args = request.args
+    station_id: str = args.get("station_id", type=str)
+    year: int = args.get("year", type=int)
+    
+    if None not in (station_id, year):
+        pass
+    elif station_id:
+        pass
+    elif year:
+        pass
+    else:
+        # return everything from the table
+        result = {}
+
+    return result
